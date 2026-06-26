82c7c6d93b95f7fe51ab1ae66945345d1af0511c
Suryansh khare <kharesuryanshkhare@gmail.com>
2026-06-27 01:09:20 +0530

    Commit 4

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
+import mongoose from "mongoose";
 import express from "express";
+import { DB_NAME } from "./constants.js";
+import dotenv from "dotenv";
+import dns from "dns"
 
+dotenv.config({path: './.env'});
 
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 const app = express();
 
-app.get('/',(req,res) => {
-    
-})
+(async() =>{
+    try{
+        const chk1 = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        // console.log(chk1)
+        app.on("error",(error)=>{
+            console.log("ERROR: ",error);
+            throw error;
+        });
+
+        app.listen(process.env.PORT || 3000,()=>{
+            console.log(`App successfully running on ${process.env.PORT}`)
+        })
+
+    }catch(error)
+    {
+        console.log("Error occured from index",error);
+        throw error;
+
+    }
+})()
