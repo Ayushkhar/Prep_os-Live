e48a0eb35c331a0625f9cab806ddc08fe7c4ea89
Suryansh khare <kharesuryanshkhare@gmail.com>
2026-07-13 03:06:08 +0530

    Add user routes and error handling utilities

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,2 +1,26 @@
-import express from "express"
-import 
+import express from "express";
+import {Router} from "express";
+
+
+const router = Router()
+
+// main router reg 
+router.route("/register").post(
+    upload.fields([
+        {
+            name: "cover image",
+            maxCount: 1
+        },
+    ]),registerUser
+)
+
+router.route("/login").post(loginUser);
+// router.route("/logout").post(logout);
+
+// Secured routes 
+router.route("/logout").post(verifyJWT, logoutUser);
+// Refresh and access token 
+router.route("/token").post()
+
+
+export default {router}
